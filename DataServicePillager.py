9e32cc164df8b9976c6a1307cbeb78c7ed05fc54
Grant Herbert <gdherbert@gmail.com>
2018-06-13 10:25:22 -0700

    simplify initial output dataset creation by replacing rename + copy step with copy only

--- DataServicePillager.py
+++ DataServicePillager.py
@@ -320,9 +320,10 @@ def combine_data(fc_list, output_fc):
             if arcpy.Exists(output_fc):
                 output_msg("Avast! {0} exists, deleting...".format(output_fc), severity=1)
                 arcpy.Delete_management(output_fc)
-            arcpy.Rename_management(fc, output_fc) # rename the first dataset to the final name
+            arcpy.CopyFeatures_management(fc, output_fc)  # create dataset to append to
+            ##arcpy.Rename_management(fc, output_fc) # rename the first dataset to the final name
             output_msg("Created {0}".format(output_fc))
-            arcpy.CopyFeatures_management(output_fc, fc) # duplicate first one so delete later doesn't fail
+            ##arcpy.CopyFeatures_management(output_fc, fc) # duplicate first one so delete later doesn't fail
             insert_rows = arcpy.da.InsertCursor(output_fc, ["SHAPE@","*"])
         else:
             search_rows = arcpy.da.SearchCursor(fc, ["SHAPE@","*"]) # append to first dataset
